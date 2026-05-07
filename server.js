ca1a30a6f7c40fbb9c807bfad7a3e02495d0d6fd
Maximo <alonsomaximo181@gmail.com>
2026-05-07 10:58:55 -0600

    primer deploy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("PUBLIC"));
 
 app.use(session({
     secret:"cyberguard123",
